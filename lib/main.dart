282170e552167722507bd7248cdc53a147183368
VIT <dtc2154801030140@ictu.edu.vn>
2024-11-26 07:13:24 +0700

    thu

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.black, width: 2),
-          ),
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(9),
-                      color: Colors.blue,
-                      border: Border.all(color: Colors.black, width: 1)),
-                  child: Center(
-                    child: Text("data"),
-                  ),
-                ),
-                Container(
-                  child: Text("data"),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24), color: Colors.black),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
+    home: myapp(),
+    debugShowCheckedModeBanner: false,
   ));
+}
+
+class myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              boxMethod("box1", Alignment.bottomRight),
+              boxMethod("box2", Alignment.bottomLeft)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              boxMethod("box3", Alignment.topRight),
+              boxMethod("box4", Alignment.topLeft)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container boxMethod(String ten, Alignment viTriChu) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.green,
+      alignment: viTriChu,
+      child: Text(
+        ten,
+        style: TextStyle(),
+      ),
+    );
+  }
 }
